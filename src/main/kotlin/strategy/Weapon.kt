2813d16fa51d47f86baed5f06ad3c0d3a04aa413
Dean Djermanovic <dean.djermanovic@five.agency>
2021-06-07 19:58:33 +0200

    Add Strategy pattern

---
+++ src/main/kotlin/strategy/Weapon.kt
@@ -0,0 +1,30 @@
+package strategy
+
+interface Weapon {
+
+    fun useWeapon()
+}
+
+class Sword : Weapon {
+    override fun useWeapon() {
+        println("Sword")
+    }
+}
+
+class Knife : Weapon {
+    override fun useWeapon() {
+        println("Knife")
+    }
+}
+
+class Axe : Weapon {
+    override fun useWeapon() {
+        println("Axe")
+    }
+}
+
+class Gun : Weapon {
+    override fun useWeapon() {
+        println("Gun")
+    }
+}
